0079b346354e056ca1af63a4486ec468d1615889
padulam <padula.matthew@gmail.com>
2016-10-02 18:05:06 -0400

    Resolve regex issue

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = process.env.PORT||8000;
 
 app.get('/',function(request,response){
-	var software = request.headers['user-agent'].match((?<=\().*?(?=\)))[0];
+	var software = request.headers['user-agent'].match(/\((.*?)\)/)[1];
 	var language = request.headers['accept-language'].split(',')[0];
 	response.json({ipaddress: request.ip, language: language, software: software});
 });
