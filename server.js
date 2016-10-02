2ca2a8256cab05ec36f4d1a85e9a88118d5f53a6
padulam <padula.matthew@gmail.com>
2016-10-02 17:43:46 -0400

    Deploy initial app

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT||8000;
+
+app.get('/',function(request,response){
+	var software = request.headers['user-agent'].match((?<=\().*?(?=\)))[0];
+	var language = request.headers['accept-language'].split(',')[0];
+	response.json({ipaddress: request.ip, language: language, software: software});
+});
+
+app.listen(port);
